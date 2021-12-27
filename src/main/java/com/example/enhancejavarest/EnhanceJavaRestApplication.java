0a0997152f78ceaff29d35a97ede30b254c08bc1
ankitk2909 <ankitkathait28@gmail.com>
2021-12-27 22:20:27 +0530

    init gradle

---
+++ src/main/java/com/example/enhancejavarest/EnhanceJavaRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.enhancejavarest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnhanceJavaRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnhanceJavaRestApplication.class, args);
+	}
+
+}
