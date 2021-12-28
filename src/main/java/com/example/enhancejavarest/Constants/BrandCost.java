78fb11885f83d2f218bdb22675d0915879a3d622
ankitk2909 <ankitkathait28@gmail.com>
2021-12-28 16:55:00 +0530

    Added coins and cost constants

---
+++ src/main/java/com/example/enhancejavarest/Constants/BrandCost.java
@@ -0,0 +1,11 @@
+package com.example.enhancejavarest.Constants;
+
+import lombok.Getter;
+
+@Getter
+public class BrandCost {
+    private final Integer costPerClick = 10;
+    private final Integer costPerView = 5;
+    private final Integer costPerSurveyQuestion = 5;
+    private final Integer conversionRatio = 10;
+}
---
+++ src/main/java/com/example/enhancejavarest/Constants/UserCoins.java
@@ -0,0 +1,10 @@
+package com.example.enhancejavarest.Constants;
+
+import lombok.Getter;
+
+@Getter
+public class UserCoins {
+    private final Integer coinsPerClick = 50;
+    private final Integer coinsPerView = 25;
+    private final Integer coinsPerQuestionInASurvey = 25;
+}
